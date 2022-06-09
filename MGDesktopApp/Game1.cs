30d17d7769aaa5895d7576b28ec85d1240591fd7
Ricardo Milan <ricardo@fluencypass.com>
2022-06-09 08:50:12 -0300

    Add project files.

---
+++ MGDesktopApp/Game1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MGDesktopApp
+{
+    internal class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+
+        public Game1() : base()
+        {
+            this.graphics = new GraphicsDeviceManager(this);
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            this.GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MGDesktopApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MGDesktopApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+}
+
+// ** Alterar o tipo do projeto de ConsoleApplication para WindowsApplication para abrir apenas a tela do jogo sem a console
